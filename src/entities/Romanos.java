99a2d3cdb2b8c5770a591c738ba3976eb873439b
trassos <tvidalo@ymail.com>
2022-04-20 10:42:58 -0300

    Funcional e com Map externo

---
+++ src/entities/Romanos.java
@@ -0,0 +1,17 @@
+package entities;
+
+import java.util.HashMap;
+
+public class Romanos {
+	public static final HashMap<String, Integer> getTabela() {
+		HashMap<String, Integer> tabela = new HashMap<String, Integer>();
+		tabela.put("I", 1);
+		tabela.put("V", 5);
+		tabela.put("X", 10);
+		tabela.put("L", 50);
+		tabela.put("C", 100);
+		tabela.put("D", 500);
+		tabela.put("M", 1000);
+		return tabela;
+	}
+}
---
+++ src/services/Conversor.java
@@ -0,0 +1,11 @@
+package services;
+
+public class Conversor(String numeroRomano) {
+	String[] letraSeparadas = numeroRomano.split("(?<=\\G.{" + 1 + "})");
+	for (int i = 0; i < letraSeparadas.length; i++) {
+		System.out.println(tabela.isEmpty());
+		System.out.println(tabela.get("I"));
+		System.out.println("OI");
+		numeros.add(tabela.get(letraSeparadas[i]));
+	}
+}
